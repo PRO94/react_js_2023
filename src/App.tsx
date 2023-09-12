787abf8fa9ef6e82ef8e798ce5caf7bf87d04d2e
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-12 13:32:18 +0200

    Add ModalContext for reusing the component and manage its state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { CreateProduct } from './components/CreateProduct';
 import { ErrorMessage } from './components/ErrorMessage';
 import { Loader } from './components/Loader';
@@ -6,14 +6,16 @@ import { Modal } from './components/Modal';
 import { Product } from './components/Product'
 import { useProducts } from './hooks/products';
 import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
 
 function App() {
 
   const { products, error, loading, addProduct } = useProducts()
-  const [ modal, setModal ] = useState(false)
+
+  const { modal, openModal, closeModal } = useContext(ModalContext)
 
   const createProductHandler = (createdProduct: IProduct) => {
-    setModal(false)
+    closeModal()
     addProduct(createdProduct)
   }
 
@@ -26,14 +28,14 @@ function App() {
       { products.map(product => <Product product={product} key={product.id}></Product>) }
 
       { modal && 
-        <Modal title='Create Product' onClose={ () => setModal(false) }>
+        <Modal title='Create Product' onClose={ () => closeModal() }>
           <CreateProduct onCreate={ createProductHandler } />
         </Modal> 
       }
 
       <button
         className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
-        onClick={ () => setModal(true ) }
+        onClick={ () => openModal() }
       >+</button>
 
     </div> 
