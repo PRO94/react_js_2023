dc4153548fec82e8ed5e01fda1b356909154361c
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-12 11:02:57 +0200

    Add example useState() usage with counter and button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React, { createElement as e } from 'react';
+import React, { createElement as e, useState } from 'react';
 
 function App() {
-  // return (
-  //   <h1>Hello from React as tags</h1>
-  // )
+  const [count, setCount] = useState(0);
 
   return e('div', {className: 'container'}, [
-    e('h1', {className: 'font-bold', key: 1}, 'Test JSX'),
-    e('button', {className: '', key: 2}, 'Click me!')
+    e('h1', {className: 'font-bold', key: 1}, `Test JSX: ${count}`),
+    e('button', {
+      className: 'py-2 px-4 border',
+      key: 2,
+      onClick: () => setCount(count + 1)
+    }, 'Click me!')
   ])
 }
 
