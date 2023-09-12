5660f7eae8d9eea798102acfc2c3ee1bd0c5486c
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-12 11:30:58 +0200

    Move example to its own file. Add first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import React, { createElement as e, useState } from 'react';
+import { Product } from './components/Product'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return e('div', {className: 'container'}, [
-    e('h1', {className: 'font-bold', key: 1}, `Test JSX: ${count}`),
-    e('button', {
-      className: 'py-2 px-4 border',
-      key: 2,
-      onClick: () => setCount(count + 1)
-    }, 'Click me!')
-  ])
+  return (
+    <div className='container mx-auto max-w-2xl pt-5'>
+      <Product/>
+    </div>
+  )
 }
 
 export default App;
