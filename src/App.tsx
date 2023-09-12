79fdbc43a29927f7fccd1c14ae5aae7c4819dad1
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-12 12:00:44 +0200

    Add Product component usage with some react.js features. Prep work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Product } from './components/Product'
+import { products } from './data/product'
 
 function App() {
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      <Product/>
+      <Product product={products[0]}/>
+      <Product product={products[1]}/>
+      <Product product={products[2]}/>
     </div>
   )
 }
