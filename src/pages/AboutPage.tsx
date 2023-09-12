4344e67cd8f09ba195a2f46a0dbbf6361c0ada5a
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-12 13:52:53 +0200

    Add react-router-dom. Add Products and About pages with navigation between them.

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+export function AboutPage() {
+    return (
+        <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis cum corporis adipisci ipsam eius nostrum quia necessitatibus atque expedita excepturi suscipit reiciendis incidunt, harum architecto error deleniti hic laborum. Iste consectetur provident fugit neque et quis quas? Dicta distinctio, ullam unde laudantium suscipit, obcaecati minus quisquam praesentium eveniet cum minima perferendis veniam quia quidem porro ea consequatur cumque corrupti maxime inventore consequuntur ducimus. Iure, numquam! Temporibus, doloremque repellendus. Voluptatem possimus recusandae in obcaecati illum enim doloremque harum. Ea facilis perferendis possimus libero eligendi ipsa aut illum maxime. Cum asperiores, quam, aliquam neque architecto, labore qui ex officia dolores officiis odit.
+        </div>
+    )
+}
---
+++ src/pages/ProductPage.tsx
@@ -0,0 +1,43 @@
+import { useContext } from 'react';
+import { CreateProduct } from '../components/CreateProduct';
+import { ErrorMessage } from '../components/ErrorMessage';
+import { Loader } from '../components/Loader';
+import { Modal } from '../components/Modal';
+import { Product } from '../components/Product'
+import { useProducts } from '../hooks/products';
+import { IProduct } from '../models';
+import { ModalContext } from '../context/ModalContext';
+
+export function ProductPage() {
+
+    const { products, error, loading, addProduct } = useProducts()
+
+    const { modal, openModal, closeModal } = useContext(ModalContext)
+  
+    const createProductHandler = (createdProduct: IProduct) => {
+      closeModal()
+      addProduct(createdProduct)
+    }
+  
+    return (
+      <div className='container mx-auto max-w-2xl pt-5'>
+        { loading && <Loader/> }
+  
+        { error && <ErrorMessage error={ error }/> }
+  
+        { products.map(product => <Product product={product} key={product.id}></Product>) }
+  
+        { modal && 
+          <Modal title='Create Product' onClose={ () => closeModal() }>
+            <CreateProduct onCreate={ createProductHandler } />
+          </Modal> 
+        }
+  
+        <button
+          className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+          onClick={ () => openModal() }
+        >+</button>
+  
+      </div> 
+    )
+}
